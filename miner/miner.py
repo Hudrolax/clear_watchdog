90cffb7f1a066235ec364bfe22235292d612644f
Hudrolax <hudro795@gmail.com>
2021-02-22 11:49:06 +0300

    change config

--- miner/miner.py
+++ miner/miner.py
@@ -112,22 +112,20 @@ class Miner(LoggerSuper):
     def _lolminer_add_cards(self):
         json = self.get_json()
         if json is None:
-            return None
-        gpus = json.get('GPUs')
-        if gpus is None:
             self.cards.append(Card(0, 0, 0, 0, 'RX 580'))
-        else:
-            for gpu in gpus:
-                card_type = gpu.get('Name')
-                if card_type.find('RX 470') > -1:
-                    card_type = 'RX 470'
-                elif card_type.find('RX 480') > -1:
-                    card_type = 'RX 480'
-                elif card_type.find('RX 570') > -1:
-                    card_type = 'RX 570'
-                elif card_type.find('RX 580') > -1:
-                    card_type = 'RX 580'
-                self.cards.append(Card(0, 0, 0, 0, card_type))
+            return
+        gpus = json.get('GPUs')
+        for gpu in gpus:
+            card_type = gpu.get('Name')
+            if card_type.find('RX 470') > -1:
+                card_type = 'RX 470'
+            elif card_type.find('RX 480') > -1:
+                card_type = 'RX 480'
+            elif card_type.find('RX 570') > -1:
+                card_type = 'RX 570'
+            elif card_type.find('RX 580') > -1:
+                card_type = 'RX 580'
+            self.cards.append(Card(0, 0, 0, 0, card_type))
 
     def _threaded_get_miner_info(self):
         # Разбор конфига
